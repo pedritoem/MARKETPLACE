72ab54dfcb1cada291ddcb70003fb8b09ebf40eb
pedritoem <josemces@gmail.com>
2023-10-03 21:38:27 -0500

    first commit

---
+++ core/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls.conf import include
+
+from .views import HomeView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+
+    #traemos las bases
+    path('users/', include('accounts.urls', namespace='users')),
+
+    path('',HomeView.as_view(), name="home"),
+
+    path('marketplace/', include('marketplace.urls', namespace="marketplace")),
+    
+]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
---
+++ marketplace/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.conf import settings
+import os
+
+#lo cambiaamos cuando estamos extendiendo un usuario, nos permite usar el foreing key del usuario
+User = settings.AUTH_USER_MODEL
+
+def marketplace_directory_path(instance, filename):
+    banner_pic_name='marketplace/products/{0}/{1}'.format(instance.name, filename)
+    full_path = os.path.join(settings.MEDIA_ROOT, banner_pic_name)
+
+    if os.path.exists(full_path):
+        os.remove(full_path)
+
+    return banner_pic_name
+
+class Product(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    thumbnail=models.ImageField(blank=True, null=True, upload_to=marketplace_directory_path)
+    slug=models.SlugField(unique=True)
+
+    content_url=models.URLField(blank=True,null=True)
+    content_file=models.FileField(blank=True, null=True)
+
+    active = models.BooleanField(default=False)
+
+    price= models.PositiveIntegerField(default=100) #cents Cant be lower than 50 cents@
+
+    #queremos ver el prod en el administrador
+    def __str__(self):
+        return self.name
+    
+    #para hacer que se vea bien el precio
+    def price_display(self):
+        return "{0:2f}".format(self.price / 100)
+    
+class PurchasedProduct(models.Model):
+    email = models.EmailField() #El email del user que compró o nuevo
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)  #Puede tener varios productos
+    date_purchased=models.DateTimeField(auto_now_add=True) #Fecha de compra
+
+    def __str__(self):
+        return self.email
